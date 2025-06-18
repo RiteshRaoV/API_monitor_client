b7f9f1f022357647fc356a7f68bfe79ca6bab549
Ritesh Rao V <ritheshraov016@gmail.com>
2025-06-18 23:27:52 +0530

    [#10] Add sample APIs to receive,decrypt incoming logs and APIs to store and retrieve projects

--- api_monitor_client/urls.py
+++ api_monitor_client/urls.py
@@ -1,14 +1,28 @@
 from django.contrib import admin
-from django.urls import path, include
-from django.conf import settings
+from django.urls import path, include, re_path
+from django.contrib import admin
+from django.urls import include, path
 
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Your API",
+        default_version='v1',
+        description="API documentation",
+    ),
+    public=True,
+)
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/v1/logs/',include('apps.logs.urls')),
+    path('api/v1/core/',include('apps.core.urls')),
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$',
+            schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    re_path(r'^swagger/$', schema_view.with_ui('swagger',
+            cache_timeout=0), name='schema-swagger-ui'),
+    re_path(r'^redoc/$', schema_view.with_ui('redoc',
+            cache_timeout=0), name='schema-redoc'),
 ]
 
-# Add this block to include debug_toolbar URLs
-if settings.DEBUG:
-    import debug_toolbar
-    urlpatterns += [
-        path('__debug__/', include(debug_toolbar.urls)),
-    ]
--- apps/core/views.py
+++ apps/core/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
 # Create your views here.
+# apps/core/views.py
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from apps.core.models import Project
+from apps.core.serializers import ProjectSerializer
+
+class ProjectListCreateAPIView(APIView):
+    def get(self, request):
+        projects = Project.objects.all()
+        serializer = ProjectSerializer(projects, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        serializer = ProjectSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
