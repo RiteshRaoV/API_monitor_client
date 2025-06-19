0510740338b2e260cf5e0c26e40a3064f062dc93
Ritesh Rao V <ritheshraov016@gmail.com>
2025-06-19 20:36:43 +0530

    [#3] Move models from logs to core app

--- apps/core/models.py
+++ apps/core/models.py
@@ -8,3 +8,16 @@ class Project(models.Model):
 
     def __str__(self):
         return self.name
+
+class Endpoint(models.Model):
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='endpoints')
+    path = models.CharField(max_length=255)  # e.g. /get-details/
+    method = models.CharField(max_length=10)  # GET, POST, etc.
+    description = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('project', 'path', 'method')
+
+    def __str__(self):
+        return f"{self.method} {self.path}"
